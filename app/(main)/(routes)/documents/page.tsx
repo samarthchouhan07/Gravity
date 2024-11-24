4ed0148941debdc1811d85e65bbbdb3e65e3f54e
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-11-24 22:05:29 +0530

    user settings done

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+    const {user}=useUser()
     return ( 
-        <div>
-            this is the protected page
+        <div className="h-full flex flex-col items-center justify-center">
+           <Image 
+           src={"/Screenshot 2024-11-24 212910.png"} height={"300"} width={"300"} alt="Empty" className="rounded-3xl"/>
+           <h2 className="text-lg font-medium">
+             Welcome to {user?.firstName}&apos; Gravity
+           </h2>
+           <Button>
+            <PlusCircle className="h-4 w-4 mr-2"/>
+            Create a note
+           </Button>
         </div>
      );
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronLeftIcon, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const isResizingRef = useRef(false);
@@ -21,7 +22,7 @@ export const Navigation = () => {
     } else {
       resetWidth();
     }
-  }, [isMobile]);
+  }, [isMobile]); 
 
   useEffect(() => {
     if (isMobile) {
@@ -112,7 +113,7 @@ export const Navigation = () => {
           <ChevronLeftIcon className="h-6 w-6" />
         </div>
         <div>
-          <p>Action Items</p>
+          <UserItem/>
         </div>
         <div className="mt-4">
           <p>Documents</p>
