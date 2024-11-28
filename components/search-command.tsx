5f8d7167d7dd071a890b0e5230447d86f37bb646
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-11-28 13:42:10 +0530

    update

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -55,16 +55,16 @@ export const SearchCommand = () => {
 
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
-      <CommandInput placeholder={`Search ${user?.fullName}'s Gravity`} />
+      <CommandInput placeholder={`Search ${user?.fullName}'s Gravity`}/>
       <CommandList>
         <CommandEmpty>No results found</CommandEmpty>
         <CommandGroup heading="Documents">
           {documents?.map((document) => (
             <CommandItem
               key={document._id}
-              value={`${document._id}`} 
+              value={`${document._id}-${document.title}`} 
               title={document.title}
-              onSelect={onSelect}
+              onSelect={()=>onSelect(document._id)}
             >
               {document.icon ? (
                 <p className="mr-2 text-[18px] ">{document.icon}</p>
